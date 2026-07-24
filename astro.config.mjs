4837ef2c21fde5dd7c7aaa362082f0361779ce0c
Joshua Sello <joshuasello@gmail.com>
2026-07-24 10:00:36 +0200

    added favicon and other SEO improvements.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     site: "https://joshuasello.com",
     output: "static",
     trailingSlash: "always",
+    integrations: [sitemap()],
     vite: {
         plugins: [tailwindcss()]
     }
